c14b7fa412351d13bc0926678ebfa51001d5e04b
Osni Pezzini Junior <osni.pezzini@gmail.com>
2017-12-04 15:34:01 -0200

    Inicio dos desafios aula 07

---
+++ desafio005.py
@@ -0,0 +1,1 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu número anterior e seu numero depois.
---
+++ desafio009.py
@@ -0,0 +1,1 @@
+# Faça um programa que leia um número inteiro qualquer e mostra na tela a sua tabuada.
