b34f17c5c8c50a4433648f56bd1a063a19fa5bf6
Osni Pezzini Junior <osni.pezzini@gmail.com>
2018-01-08 13:33:21 -0200

    Resolução exercicio 38

--- ex038.py
+++ ex038.py
@@ -4,3 +4,13 @@ Escreva um programa que leia dois numeros inteiros e compare-os , mostrando na t
 - O segundo valor é maior
 - Não existe valor maior , os dois são iguais 
 '''
+
+num1 = int(input('Digite o primeiro numero '))
+num2 = int(input('Digite o segundo numero'))
+
+if num1 < num2:
+    print('O primeiro numero é menor que o segundo numero')
+elif num1 > num2:
+    print('O primeiro numero é maior que o segundo numero')
+elif num1 == num2:
+    print('Os dois numeros são iguais')
