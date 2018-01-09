a46a45988cb59ce73bb70909affb9840ab26e318
Osni Pezzini Junior <osni.pezzini@gmail.com>
2018-01-09 13:17:26 -0200

    Resolução exercicio 36 e 37

--- ex036.py
+++ ex036.py
@@ -1,4 +1,15 @@
- Escreva um programa para aprovar o empréstimo para a compra de uma casa. O programa vai perguntar o valor da casa , o salario do comprador e em quantos anos ele vai pagar:
+'''Escreva um programa para aprovar o empréstimo para a compra de uma casa. O programa vai perguntar o valor da casa , o salario do comprador e em quantos anos ele vai pagar:
 
  Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30%¨do salário ou então emprestimo será negado.
+'''
 
+valor = float(input('Digite o valor da casa : '))
+salario = float(input('Digite seu sálário : '))
+parcela = int(input('Em quantas vezes quer pagar ? '))
+#perce = salario
+presta = valor / parcela
+
+if ((salario * 30) / 100) < parcela:
+    print('\033[31m'+'Empréstimo negado')
+else:
+    print('O valor da prestação mensal é de {:.2f}'.format(presta))
--- ex037.py
+++ ex037.py
@@ -5,3 +5,21 @@ Escreva um programa que leia um numero inteiro qualquer e peça para o usuário 
 - 2 para octal
 - 3 para hexadecimal
 '''
+
+def main():
+    num = int(input('Digite um numero qualquer : '))
+
+    print('Digite 1 para converter para binário \nDigite 2 para converter para Octal \nDigite 3 para converter para hexadecimal\n')
+    escolha = int(input('Selecione uma opção : '))
+
+    if escolha == 1:
+        print(bin(num))
+    elif escolha == 2:
+        print(oct(num))
+    elif escolha == 3:
+        print(hex(num))
+    else:
+        print('\033[31m'+'Escolha uma opção válida'+'\033[0;0m')
+
+while True:
+    main()
