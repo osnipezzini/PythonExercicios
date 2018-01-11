4fc583b6b5e5a80de20ad4108f680c4238fe42ec
Osni Pezzini Junior <osni.pezzini@gmail.com>
2018-01-11 13:38:21 -0200

    Resolução ex044

--- ex044.py
+++ ex044.py
@@ -6,3 +6,24 @@ Elabore um programa que calcule o valor a ser pago por um produto , considerando
 - em até 2x no cartão : preço normal
 - 3x ou mais no cartão: 20% de juros
 '''
+
+preco = float(input('Qual o valor do produto ? '))
+print('Digite 1 para pagamento em dinheiro. \nDigite 2 para pagar no cartão.')
+escolha = int(input('Digite a opção desejada ! '))
+
+
+if escolha == 1:
+    preconovo = preco - (preco * 10 / 100)
+    print('O valor a ser pago é de {}'.format(preconovo))
+elif escolha == 2:
+    vezes = int(input('Em quantas vezes deseja parcelar ? '))
+    if vezes == 1:
+        preconovo = preco - (preco * 5 / 100)
+        print('O valor a ser pago é de {} '.format(preconovo))
+    elif vezes == 2:
+        print('O preço a ser pago é de {} dividido em {} vezes '.format(preco, vezes))
+    else:
+        preconovo = preco + (preco * 20 / 100)
+        print('O valor a ser pago é de {} , dividido em {} vezes'.format(preconovo, vezes))
+else:
+    print('Digite uma opção válida!')
--- ex045.py
+++ ex045.py
@@ -1,3 +1,4 @@
 '''
 Crie um programa que faça o computador jogar Jokenpô com você
 '''
+
