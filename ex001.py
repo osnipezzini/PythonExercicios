54d488a47589506ad2ad29a6ba27d0b690281ba1
Osni Pezzini Junior <osni.pezzini@gmail.com>
2018-01-29 14:03:15 -0200

    Resolução ex045

--- ex001.py
+++ ex001.py
@@ -1,2 +1,8 @@
-msg = 'Olá, Mundo !'
-print(msg)
+cont = 1
+while True:
+    print(cont, '-> ', end='')
+    cont += 1
+    if cont >= 10:
+        break
+
+print(f'Contador igual {cont}')
---
+++ ex070.py
@@ -0,0 +1,7 @@
+'''
+Crie um programa que leia o nome e o preço de vários produtos. o programa deverá perguntar se o usuário vai continuar. No final, mostre:
+
+* Qual é o total gasto na compra
+* Quantos produtos custam mais de R$1000
+* Qual é o nome do produto mais barato.
+'''
