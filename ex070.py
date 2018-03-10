eeff446f278c28afe91c196cf9285b7a3e975112
Osni Pezzini Junior <osni.pezzini@gmail.com>
2018-03-10 15:38:39 -0300

    Correção dos exercicios

--- ex070.py
+++ ex070.py
@@ -6,18 +6,14 @@ Crie um programa que leia o nome e o preço de vários produtos. o programa deve
 * Qual é o nome do produto mais barato.
 '''
 
-total = caro = barato = precobarato = soma = cont = 0
+caro = barato = precobarato = soma = cont = 0
 while True:
     nome = input('Digite o nome do produto: ')
     preco = float(input('Digite o preço do produto: R$ '))
     opcao = input('Deseja continuar ? S/N').upper().strip()
-    if cont == 0:
+    if cont == 0 or preco < precobarato:
         barato = nome
         precobarato = preco
-        total = preco
-    if preco < precobarato:
-        precobarato = preco
-        barato = nome
     if preco >= 1000:
         caro += 1
     cont += 1
@@ -26,6 +22,6 @@ while True:
         break
 print(f'''
 Você cadastrou {cont} produtos , dentre eles :
-{caro} produtos custam mais de R$ 1000,00
+{caro} produtos custam mais de R$ 1000.00
 {barato} é o produto mais barato , custando apenas R$ {precobarato:.2f}
 Foram gastos R$ {soma:.2f} nessa compra.''')
--- ex071.py
+++ ex071.py
@@ -3,20 +3,32 @@ Crie um programa que simule o funcionamento de um caixa eletrônico. No inicio, 
 
 OBS: Considere que o caixa possui cédulas de R$ 50, R$20, R$10 e R$1
 '''
-
+print('=' * 30)
+print('{:^30}'.format('BANCO ELLITE'))
+print('=' * 30)
 cinquenta = vinte = dez = um = 0
 sacar = int(input('Quanto quer sacar ?'))
-while sacar >= 50:
-    sacar -= 50
-    cinquenta += 1
-while sacar >= 20:
-    sacar -= 20
-    vinte += 1
-while sacar >= 10:
-    sacar -= 10
-    dez += 1
-while sacar >= 1:
-    um += 1
-    sacar -= 1
-print(f'''
-Vou lhe entregar {cinquenta} notas de R$ 50,00 , {vinte} notas de R$ 20,00 , {dez} notas de R$10,00 e {um} notas de R$ 1,00''')
+while True:
+    if sacar >= 50:
+        sacar -= 50
+        cinquenta += 1
+    elif sacar >= 20:
+        sacar -= 20
+        vinte += 1
+    elif sacar >= 10:
+        sacar -= 10
+        dez += 1
+    elif sacar >= 1:
+        um += 1
+        sacar -= 1
+    else:
+        break
+
+if cinquenta != 0:
+    print(f'{cinquenta} cédulas de R$50.00')
+if vinte != 0:
+    print(f'{vinte} cédulas de R$20.00')
+if dez != 0:
+    print(f'{dez} cédulas de R$10.00')
+if um != 0:
+    print(f'{um} cédulas de R$1.00')
